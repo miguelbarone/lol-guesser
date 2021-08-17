afeb7740a26ac0e612a7ecfddb92d58b86e17443
miguelbarone <miguel.barone@picpay.com>
2021-08-16 21:08:42 -0300

    create xcode project

---
+++ LeagueOfLegendsGuesser/LeagueOfLegendsGuesser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeagueOfLegendsGuesser
+//
+//  Created by Miguel Barone on 16/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
